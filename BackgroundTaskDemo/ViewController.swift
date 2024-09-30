3911a52a5b6cd1bc76d2e0691936b16a03e7cddc
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-30 12:56:36 -0600

    Initial Commit

---
+++ BackgroundTaskDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundTaskDemo
+//
+//  Created by Marco Alonso on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
